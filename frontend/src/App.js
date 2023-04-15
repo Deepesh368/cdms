163f75c93ad1038c672c907285ea5a823b133b5e
Deepesh368 <kvvdeepesh@gmail.com>
2023-04-15 12:41:38 +0530

    Added navbar for securityDashboard

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,24 @@
-import React from 'react';
-import { Container } from '@material-ui/core';
+import React from "react";
+import { Container } from "@material-ui/core";
 
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
-import Login from './components/Login/Login';
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Login from "./components/Login/Login";
+import Securitydashboard from "./components/securityDashboard/securityDashboard";
 
 const App = () => {
-  
-    return (
-        <BrowserRouter>
-            <Container maxWidth="xl">
-                <Routes>
-                <Route path="/" element={<Login />}>
-                </Route>
-                </Routes>
-            </Container>
-        </BrowserRouter>
-    );
+  return (
+    <BrowserRouter>
+      <Container maxWidth="xl">
+        <Routes>
+          <Route path="/" element={<Login />}></Route>
+          <Route
+            path="/securityDashboard"
+            element={<Securitydashboard />}
+          ></Route>
+        </Routes>
+      </Container>
+    </BrowserRouter>
+  );
 };
 
 export default App;
---
+++ frontend/src/components/securityDashboard/Navbar.js
@@ -0,0 +1,46 @@
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+import { AppBar, Toolbar, Typography, Button } from "@mui/material";
+import LocalShippingIcon from "@mui/icons-material/LocalShipping";
+
+const Header = () => {
+  let navigate = useNavigate();
+
+  const logout = () => {
+    return;
+  };
+
+  return (
+    <React.Fragment>
+      <AppBar sx={{ background: "#10BB40", minHeight: "70px", marginBottom: "111px" }}>
+        <Toolbar>
+          <Button
+            // color="primary"
+            sx={{ color: "white" }}
+            size="large"
+            startIcon={<LocalShippingIcon />}
+            onClick={() => navigate("/")}
+          >
+            College Delivery Management System
+          </Button>
+          <Button
+            sx={{
+              "&:hover": { backgroundColor: "#8A0717" },
+              marginLeft: "auto",
+              borderRadius: "20px",
+              background: "red",
+              color: "white",
+            }}
+            variant="contained"
+            onClick={logout}
+          >
+            Log out
+          </Button>
+        </Toolbar>
+      </AppBar>
+    </React.Fragment>
+  );
+};
+
+export default Header;
